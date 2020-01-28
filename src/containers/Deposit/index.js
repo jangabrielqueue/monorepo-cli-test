8bd3b65f4f2a2912555b7c65d771b5956ae959f7
Clement <tanclement128@gmail.com>
2020-01-28 17:24:47 +0800

    tidy up

--- src/containers/Deposit/index.js
+++ src/containers/Deposit/index.js
@@ -43,7 +43,6 @@ const Deposit = props => {
       error: undefined,
       progress: undefined,
     });
-
     const result = await sendDepositRequest({
       ...values,
       reference: queryParams.get("reference"),
