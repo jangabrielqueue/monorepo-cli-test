2c887c4d86a499e2298c015651e6f6f39a058bec
Naicky <naicky.serrano@whiterain-dti.com>
2020-04-06 18:01:46 +0800

    wip help to reduce bundle size

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,11 @@
-const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const { override, fixBabelImports, addWebpackPlugin } = require('customize-cra');
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: true,
-    }),
-    addLessLoader({
-        javascriptEnabled: true
+        style: 'css',
     }),
     addWebpackPlugin(
         new AntdDayjsWebpackPlugin()
