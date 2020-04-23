6db61b6c80893faecd8f21baed5353c84fb23e06
Naicky <naicky.serrano@whiterain-dti.com>
2020-04-23 22:37:49 +0800

    fix additonal modal styling

--- src/containers/TopUp/index.js
+++ src/containers/TopUp/index.js
@@ -332,10 +332,7 @@ const TopUp = props => {
       }
         <ConfirmationModal visible={progress && (progress.statusCode === '009')}>
           <div className='progress-bar-container'>
-            {
-              (progress && (progress.currentStep / progress.totalSteps) * 100) <= 100 &&
-              <img alt='submit-transaction' width='80' src={require('../../assets/icons/in-progress.svg')} />
-            }
+            <img alt='submit-transaction' width='80' src={require('../../assets/icons/in-progress.svg')} />
             <Progress
               percent={progress && (progress.currentStep / progress.totalSteps) * 100}
               status='active'
