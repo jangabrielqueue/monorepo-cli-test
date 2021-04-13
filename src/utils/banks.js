d935dbd227e0094db709debea65eae5a27bb4570
naicky-whiterain <naicky.serrano@whiterain-dti.com>
2021-04-13 14:52:20 +0800

    added bni new theme

--- src/utils/banks.js
+++ src/utils/banks.js
@@ -26,6 +26,7 @@ const CIMB = { code: 'CIMB', name: 'CIMB Niaga' }
 const MANDIRI = { code: 'MANDIRI', name: 'MANDIRI Bank' }
 const BRI = { code: 'BRI', name: 'Bank BRI' }
 const BCA = { code: 'BCA', name: 'Bank Central Asia' }
+const BNI = { code: 'BNI', name: 'Bank Negara Indonesia' }
 
 const VND_ALL_BANKS = [VCB, ACB, VTB, DAB, BIDV, TCB, EXIM, SACOM, AGRI]
 const VND_ALL_BANKS_DEV = [
@@ -50,8 +51,8 @@ const VND_TOPUP_BANKS_DEV = [VCB, ACB, TCB, BIDV, SACOM, FAKER]
 const THB_TOPUP_BANKS = [KBANK, KTB, SCB, BBL, BOA, TMB]
 const THB_TOPUP_BANKS_DEV = [KBANK, KTB, SCB, BBL, BOA, TMB, FAKERTHB]
 
-const IDR_ALL_BANKS = [PERMATA, CIMB, MANDIRI, BRI, BCA]
-const IDR_ALL_BANKS_DEV = [PERMATA, CIMB, MANDIRI, BRI, BCA]
+const IDR_ALL_BANKS = [PERMATA, CIMB, MANDIRI, BRI, BCA, BNI]
+const IDR_ALL_BANKS_DEV = [PERMATA, CIMB, MANDIRI, BRI, BCA, BNI]
 
 function getVndBanksByEnvForDeposit () {
   if (isUatOrDev) {
