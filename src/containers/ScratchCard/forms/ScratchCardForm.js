f46095f13b3a0d9fb83d90a016c6ff06db3468b5
Naicky <naicky.serrano@whiterain-dti.com>
2021-05-12 12:32:02 +0800

    changed viettel to 24%

--- src/containers/ScratchCard/forms/ScratchCardForm.js
+++ src/containers/ScratchCard/forms/ScratchCardForm.js
@@ -279,7 +279,7 @@ const ScratchCardForm = React.memo((props) => {
     if (language === 'vi-vn') {
       return (
         <ul>
-          <li>VIETTEL: 28%</li>
+          <li>VIETTEL: 24%</li>
           <li>MOBI: 27% (Giá trị thẻ 10-20-30-50-100) và 30% (Giá trị thẻ 200-300-500)</li>
           <li>VINA: 25%</li>
           <li>ZING: 25%</li>
@@ -290,7 +290,7 @@ const ScratchCardForm = React.memo((props) => {
     } else {
       return (
         <ul>
-          <li>VIETTEL: 28%</li>
+          <li>VIETTEL: 24%</li>
           <li>MOBI: 27% (card value 10-20-30-50-100) and 30% (card value 200-300-500)</li>
           <li>VINA: 25%</li>
           <li>ZING: 25%</li>
