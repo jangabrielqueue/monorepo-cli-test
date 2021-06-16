a63da4275067d20b785b6868f9bb50ed97a32f02
Naicky <naicky.serrano@whiterain-dti.com>
2021-06-16 12:50:26 +0800

    updated transaction fee rate scratch card

--- src/containers/ScratchCard/forms/ScratchCardForm.js
+++ src/containers/ScratchCard/forms/ScratchCardForm.js
@@ -279,9 +279,9 @@ const ScratchCardForm = React.memo((props) => {
     if (language === 'vi-vn') {
       return (
         <ul>
-          <li>VIETTEL: 24%</li>
+          <li>VIETTEL: 25%</li>
           <li>MOBI: 27% (Giá trị thẻ 10-20-30-50-100) và 30% (Giá trị thẻ 200-300-500)</li>
-          <li>VINA: 25%</li>
+          <li>VINA: 23%</li>
           <li>ZING: 25%</li>
           <li>GATE: 28%</li>
           <li>VIETNAMOBILE: 25%</li>
@@ -290,9 +290,9 @@ const ScratchCardForm = React.memo((props) => {
     } else {
       return (
         <ul>
-          <li>VIETTEL: 24%</li>
+          <li>VIETTEL: 25%</li>
           <li>MOBI: 27% (card value 10-20-30-50-100) and 30% (card value 200-300-500)</li>
-          <li>VINA: 25%</li>
+          <li>VINA: 23%</li>
           <li>ZING: 25%</li>
           <li>GATE: 28%</li>
           <li>VIETNAMOBILE: 25%</li>
