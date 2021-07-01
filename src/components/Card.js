18c9ff1a7582a8f01e77568620510e28802231aa
Tan <tanclement128@gmail.com>
2021-07-01 16:53:01 +0800

    use react-jss for stylings

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  cardContainer: {
+    margin: '0 20px',
+    maxWidth: '500px',
+    width: '100%',
+    background: '#FFFFFF',
+    borderRadius: '15px',
+    boxShadow: '0px 5px 10px 0px rgba(112,112,112,0.3)',
+    padding: '20px'
+  }
+})
 
 export const Card = ({ children }) => {
+  const classes = useStyles()
   return (
-    <div style={{
-      margin: '0 20px',
-      maxWidth: '500px',
-      width: '100%',
-      background: '#FFFFFF',
-      borderRadius: '15px',
-      boxShadow: '0px 5px 10px 0px rgba(112,112,112,0.3)',
-      padding: '20px'
-    }}
-    >
+    <div className={classes.cardContainer}>
       {children}
     </div>
   )
