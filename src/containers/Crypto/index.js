ee8b57610d6083513069082b17e65c3f931463ca
Jan Gabriel Que(Gab) <jan.que@whiterain-dti.com>
2023-03-30 19:03:49 +0800

    minor adjustment translation

--- src/containers/Crypto/index.js
+++ src/containers/Crypto/index.js
@@ -298,7 +298,7 @@ const UsdtPage = (props) => {
           </div>
           <div style={{ fontSize: 12, display: 'flex', flexDirection: 'column' }}>
             {
-              conversion == null ? <LoadingIcon /> : <>1 {crypto} ~ {new Intl.NumberFormat(language).format(conversion.toFixed(2))} {currency}<FormattedMessage {...messages.notes.expectedRateTitle} /></>
+              conversion == null ? <LoadingIcon /> : <>1 {crypto} ~ {new Intl.NumberFormat(language).format(conversion.toFixed(2))} {currency} <FormattedMessage {...messages.notes.expectedRateTitle} /></>
             }
             <div style={{ fontSize: 10 }}>
               <b><FormattedMessage {...messages.notes.noteTitle} /></b> <FormattedMessage {...messages.notes.expectedRate} />
